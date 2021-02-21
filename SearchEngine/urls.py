f2fa38f4057097fc515770861922e778421d8ce2
Roshan <lamichhaner40@gmail.com>
2021-02-20 23:10:22 -0500

    Improved The Frontend Design

--- SearchEngine/urls.py
+++ SearchEngine/urls.py
@@ -1,9 +1,22 @@
+"""
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index), # Index Page - Tell Users To Create an Account or Login In
-    url(r'^login$', views.login),
+    url(r'^$', views.index), # Index Page - User Will Login
     url(r'^signup$', views.register),
-    url(r'^home$', views.home)
+    url(r'^home$', views.home),
+    url(r'^user_login$', views.user_login)
+]
+"""
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import user_signup, IndexView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='home'),
+    path('signup/', user_signup, name='user_signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'),name='user_login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
--- SearchEngine/views.py
+++ SearchEngine/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.views.decorators.csrf import csrf_protect
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView
 
 def index(request):
     return render(request, 'index.html')
-
-def login(request):
-    return render(request, 'login.html')
 def register(request):
     return render(request, 'signup.html')
 def home(request):
     return render(request, 'home.html')
+
+def user_signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def user_login(request):
+    pass
+
+class IndexView(TemplateView):
+    template_name = "index.html"
