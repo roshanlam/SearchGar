abee8b8a6f290e1e50cb8098d5cf66771a79eebb
Roshan <lamichhaner40@gmail.com>
2021-02-22 23:12:42 -0500

    Started working on the crawler and logger

---
+++ SearchEngine/crawler.py
@@ -0,0 +1,11 @@
+from Crawler import *
+"""
+ToDo: 
+Only crawl websites that the admin(me) adds to list of websites, this can save space and improve performance.
+But not all the websites will be added by the admin, only websites that do not have the domain extensions like .gov, .org, .edu will be added by 
+me. This is to include only reliable and credible sources where people can easily find useful information. 
+This might change in the future, so the goal is to code the crawler in such a way that if I decide to let the crawler crawl every single 
+domain name available then it will do without me having to change that much code, hopefully I will only have to change one line of code.
+"""
+class Crawler:
+    pass
